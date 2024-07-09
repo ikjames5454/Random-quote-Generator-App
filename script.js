4633aa063ebc2f3bacb878631df5fe616eae7dbf
ikjames5454 <ikennajames03@gmail.com>
2024-07-09 12:36:25 +0100

    add

--- script.js
+++ script.js
@@ -48,10 +48,68 @@
  
 //   setInterval(containers, 30000);
 // };
+// const randomQuote = document.getElementById('RandomQuote');
+// const author = document.getElementById('authors');
+// const button = document.getElementById('Button');
+// const sound = document.querySelector('.sound');
+
+// const containers = async () => {
+//   button.innerText = 'Generating Quote';
+//   try {
+//     console.log('1');
+//     const response = await fetch('https://quotable.io/random');  
+//     const json = await response.json();
+//     console.log(json);
+    
+//     func(json);
+//     playSound();
+//     button.innerText = 'Generate';
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//   } finally {
+//     setTimeout(() => {
+//       button.style.backgroundColor = '';
+//       button.style.color = '';
+//     });
+//   }
+// };
+
+// const func = (place) => {
+//   author.innerText = place.author;
+//   randomQuote.innerText = place.content;
+// };
+
+// const playSound = () => {
+//   let speech = new SpeechSynthesisUtterance(`${randomQuote.innerText} by ${author.innerText}`);
+//   speechSynthesis.speak(speech);
+// };
+
+// button.onclick = () => {
+//   button.style.backgroundColor = 'black';
+//   button.style.color = 'white'; 
+//   containers();
+// };
+
+// sound.onclick = playSound;
+
+
+// window.onload = () => {
+//   containers();
+  
+//   setInterval(containers, 30000);
+// };
 const randomQuote = document.getElementById('RandomQuote');
 const author = document.getElementById('authors');
 const button = document.getElementById('Button');
 const sound = document.querySelector('.sound');
+
+let selectedVoice;
+
+
+const setVoice = () => {
+  const voices = speechSynthesis.getVoices();
+  selectedVoice = voices.find(voice => voice.name === 'Google UK English Male') || voices[0];
+};
 
 const containers = async () => {
   button.innerText = 'Generating Quote';
@@ -81,6 +139,7 @@ const func = (place) => {
 
 const playSound = () => {
   let speech = new SpeechSynthesisUtterance(`${randomQuote.innerText} by ${author.innerText}`);
+  speech.voice = selectedVoice;
   speechSynthesis.speak(speech);
 };
 
@@ -94,7 +153,11 @@ sound.onclick = playSound;
 
 
 window.onload = () => {
+  setVoice();
   containers();
   
   setInterval(containers, 30000);
 };
+
+
+speechSynthesis.onvoiceschanged = setVoice;
