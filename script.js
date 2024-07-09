2f8b3fb001b2e68d8d6ee5afeeb9a1a4cb8f0a80
ikjames5454 <ikennajames03@gmail.com>
2024-07-09 12:25:32 +0100

    origin main

--- script.js
+++ script.js
@@ -1,42 +1,100 @@
+// const randomQuote = document.getElementById('RandomQuote');
+// const author = document.getElementById('authors');
+// const button = document.getElementById('Button');
+// const sound = document.querySelector('.sound');
+
+
+// const containers = async () => {
+//   button.innerText = 'Generating Quote';
+//   try {
+//     console.log('1')
+//     const response = await fetch('https://quotable.io/random');
+//     const json = await response.json();
+//     console.log(json)
+    
+//     func(json);
+//     button.innerText = 'Generate'
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//   } finally{
+//     setTimeout(() => {
+//       button.style.backgroundColor = '';
+//       button.style.color = ''})
+//   }
+// };
+
+
+// const func = (place) => {
+//      author.innerText = place.author
+//      randomQuote.innerText = place.content
+
+// }
+
+// button.onclick = () => {
+//   button.style.backgroundColor = 'black';
+//   button.style.color = 'white'; 
+//  containers()}
+
+//  const playSound = () => {
+//   let speech = new SpeechSynthesisUtterance(`${randomQuote.innerText} by ${author.innerText}`);
+//   speechSynthesis.speak(speech);
+
+//  }
+
+//  sound.onclick = playSound;
+
+//  window.onload = () => {
+//   containers();
+ 
+//   setInterval(containers, 30000);
+// };
 const randomQuote = document.getElementById('RandomQuote');
 const author = document.getElementById('authors');
 const button = document.getElementById('Button');
 const sound = document.querySelector('.sound');
 
-
 const containers = async () => {
   button.innerText = 'Generating Quote';
   try {
-    console.log('1')
-    const response = await fetch('http://quotable.io/random');
+    console.log('1');
+    const response = await fetch('https://quotable.io/random');  
     const json = await response.json();
-    console.log(json)
+    console.log(json);
     
     func(json);
-    button.innerText = 'Generate'
+    playSound();
+    button.innerText = 'Generate';
   } catch (error) {
     console.error('Error fetching data:', error);
-  } finally{
+  } finally {
     setTimeout(() => {
       button.style.backgroundColor = '';
-      button.style.color = ''})
+      button.style.color = '';
+    });
   }
 };
 
-
 const func = (place) => {
-     author.innerText = place.author
-     randomQuote.innerText = place.content
+  author.innerText = place.author;
+  randomQuote.innerText = place.content;
+};
 
-}
+const playSound = () => {
+  let speech = new SpeechSynthesisUtterance(`${randomQuote.innerText} by ${author.innerText}`);
+  speechSynthesis.speak(speech);
+};
 
 button.onclick = () => {
   button.style.backgroundColor = 'black';
   button.style.color = 'white'; 
- containers()}
+  containers();
+};
 
- sound.onclick = () => {
-  let speech = new SpeechSynthesisUtterance(`${randomQuote.innerText} by ${author.innerText}`);
-  speechSynthesis.speak(speech);
+sound.onclick = playSound;
 
- }
+
+window.onload = () => {
+  containers();
+  
+  setInterval(containers, 30000);
+};
